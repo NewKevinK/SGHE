520b6003492a006e6cc4f0faee8253b23fd487ab
NewKevinK <zS19014047@estudiantes.uv.mx>
2022-10-19 17:37:22 -0500

    Cliente y Logica

---
+++ Client/InicioSesion.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGHE.Client
+{
+    /// <summary>
+    /// Lógica de interacción para InicioSesion.xaml
+    /// </summary>
+    public partial class InicioSesion : Window
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+    }
+}
