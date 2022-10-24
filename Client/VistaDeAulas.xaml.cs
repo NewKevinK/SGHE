d7db97360a5a2a62841a58464903eac405b751fe
Samuel <samy.srzcln@gmail.com>
2022-10-24 10:38:58 -0500

    Agregacion del registro de horarios y vista de las aulas

---
+++ Client/VistaDeAulas.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SGHE.Client
+{
+    /// <summary>
+    /// Lógica de interacción para VistaDeAulas.xaml
+    /// </summary>
+    public partial class VistaDeAulas : Window
+    {
+        public VistaDeAulas()
+        {
+            InitializeComponent();
+        }
+    }
+}
