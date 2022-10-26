5b3fdf38d19a91461e058d149eaba08b6fccb48a
Steban <estebanelder4@gmail.com>
2022-10-26 18:54:44 -0500

    Actualización

---
+++ LogicaNegocio/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGHE.LogicaNegocio
+{
+    public abstract class Usuario
+    {
+        private int idUsuario;
+        private string nombre;
+        private string apellidoPaterno;
+        private string apellidoMaterno;
+        private DateTime fechaNacimiento;
+        private string telefono;
+        private string email;
+        private string domicilio;
+        private string password;
+        private int idTipoUusario;
+
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string ApellidoPaterno { get => apellidoPaterno; set => apellidoPaterno = value; }
+        public string ApellidoMaterno { get => apellidoMaterno; set => apellidoMaterno = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Email { get => email; set => email = value; }
+        public string Domicilio { get => domicilio; set => domicilio = value; }
+        public string Password { get => password; set => password = value; }
+        public int IdTipoUusario { get => idTipoUusario; set => idTipoUusario = value; }
+    }
+}
