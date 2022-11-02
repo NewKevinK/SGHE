89ed23a9603b4cc16962bbf1cb1530a99ac17d9c
Steban <estebanelder4@gmail.com>
2022-11-02 13:31:03 -0600

    Conexión a base de datos

--- ConexionBaseDatos/ConexionBD.cs
+++ ConexionBaseDatos/ConexionBD.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using SGHE.ConexionBD;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SGHE.ConexionBD
+namespace SGHE.ConexionBaseDatos
 {
-    internal interface ConexionBD
+    public class ConexionBD : IConexionBD
     {
+        private string SERVIDOR = "mysql-flexible-service.mysql.database.azure.com";
+        private string PUERTO = "3306";
+        private string BASE_DATOS = "sghe";
+        private string USUARIO_BD = "s19isof";
+        private string PASSWORD = "mysql-isof2022";
+
+        
+        MySqlConnection IConexionBD.ObtenerConexion()
+        {
+            MySqlConnection conexion;
+            try
+            {
+                string urlConexion = string.Format("server={0};" +
+                                                   "database={1};" +
+                                                   "username={2};" +
+                                                   "password={3};" +
+                                                   "port={4};", SERVIDOR, BASE_DATOS,
+                                                   USUARIO_BD, PASSWORD, PUERTO);
+                conexion = new MySqlConnection(urlConexion);
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                conexion = null;
+            }
+            return conexion;
+        }
     }
 }
---
+++ ConexionBaseDatos/IConexionBD.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SGHE.ConexionBD
+{
+    public interface IConexionBD
+    {
+        private static string SERVIDOR;
+        private static string PUERTO;
+        private static string BASE_DATOS;
+        private static string USUARIO_BD;
+        private static string PASSWORD;
+
+        public MySqlConnection ObtenerConexion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
