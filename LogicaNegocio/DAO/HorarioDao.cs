a3b1150b4ff75c787dc108b520cc3b422fc736a2
Eduardo Castillo <antony.9707@gmail.com>
2022-12-13 12:01:37 -0600

    cambios eduardo

--- LogicaNegocio/DAO/HorarioDao.cs
+++ LogicaNegocio/DAO/HorarioDao.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using SGHE.LogicaNegocio.POCO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SGHE.LogicaNegocio.DAO
 {
-    internal class HorarioDao
+    public class HorarioDao
     {
+
+        #region QUERYS 
+
+        #region QUERY_RECUPERAR_HORARIO_ALUMNO_DIASEMANA
+        public static readonly string QUERY_RECUPERAR_HORARIO_ALUMNO_DIASEMANA =
+            "SELECT " +
+            "experienciaeducativa.idEE," +
+            " experienciaeducativa.NRC," +
+            " experienciaeducativa.nombre AS EE," +
+            " creditos," +
+            " modalidad," +
+            " docenteA.nombre AS docente," +
+            " docenteA.apellidoPaterno AS apellidoPaterno," +
+            " docenteA.apellidoMaterno AS apellidoMaterno," +
+            " horaInicio, horaFin, diaSemana, codigoAula" +
+            "FROM experienciaeducativa" +
+            " LEFT JOIN (SELECT idDocente, nombre, apellidoPaterno, apellidoMaterno" +
+            "FROM docente JOIN usuario" +
+            "ON docente.idUsuario = usuario.idUsuario)" +
+            "AS docenteA" +
+            "ON docenteA.idDocente = experienciaeducativa.idDocente" +
+            "LEFT JOIN (SELECT idEE, horaInicio, horaFin, diaSemana, aula.codigoAula" +
+            "FROM horario" +
+            "JOIN aula" +
+            "ON horario.idAula = aula.idAula" +
+            ") AS horarioAula" +
+            "ON horarioAula.idEE = experienciaeducativa.idEE" +
+            "LEFT JOIN participantes" +
+            "ON experienciaeducativa.idEE = participantes.idEE" +
+            "LEFT JOIN periodo" +
+            "ON experienciaeducativa.idPeriodo = periodo.idPeriodo" +
+            "WHERE experienciaeducativa.idPeriodo = {idPeriodo} AND horarioAula.diaSemana = {diaSemana} AND participantes.idAlumno = {idAlumno}";
+        #endregion QUERY_RECUPERAR_HORARIO_ALUMNO_DIASEMANA
+
+        #endregion QUERYS
+
+
+        public static HorarioDiaEE RecuperarHorarioAlumnoPorDia(int periodo, int diaSemana, int idAlumno)
+        {
+            HorarioDiaEE horarioDiaEE = null;
+
+
+
+            return horarioDiaEE;
+        }
     }
 }
---
+++ LogicaNegocio/POCO/HorarioDiaEE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGHE.LogicaNegocio.POCO
+{
+    public class HorarioDiaEE
+    {
+        public int IdEE { get; set; }
+        public string NRC { get; set; }
+        public string NombreEE { get; set; }
+        public int Creditos { get; set; }
+        public string Modalidad { get; set; }
+        public string NombreCompletoDocente { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
+        public int DiaSemana { get; set; }
+        public string CodigoAula { get; set; }
+    }
+}
