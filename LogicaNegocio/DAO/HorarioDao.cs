56c08a585c2698245e0714fecad88ef051ea86a7
Eduardo Castillo <antony.9707@gmail.com>
2022-12-13 13:26:25 -0600

    horario dao

--- LogicaNegocio/DAO/HorarioDao.cs
+++ LogicaNegocio/DAO/HorarioDao.cs
@@ -1,6 +1,9 @@
-﻿using SGHE.LogicaNegocio.POCO;
+﻿using MySql.Data.MySqlClient;
+using SGHE.ConexionBaseDatos;
+using SGHE.LogicaNegocio.POCO;
 using System;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace SGHE.LogicaNegocio.DAO
@@ -22,35 +25,69 @@ namespace SGHE.LogicaNegocio.DAO
             " docenteA.apellidoPaterno AS apellidoPaterno," +
             " docenteA.apellidoMaterno AS apellidoMaterno," +
             " horaInicio, horaFin, diaSemana, codigoAula" +
-            "FROM experienciaeducativa" +
+            " FROM experienciaeducativa" +
             " LEFT JOIN (SELECT idDocente, nombre, apellidoPaterno, apellidoMaterno" +
-            "FROM docente JOIN usuario" +
-            "ON docente.idUsuario = usuario.idUsuario)" +
-            "AS docenteA" +
-            "ON docenteA.idDocente = experienciaeducativa.idDocente" +
-            "LEFT JOIN (SELECT idEE, horaInicio, horaFin, diaSemana, aula.codigoAula" +
-            "FROM horario" +
-            "JOIN aula" +
-            "ON horario.idAula = aula.idAula" +
+            " FROM docente JOIN usuario" +
+            " ON docente.idUsuario = usuario.idUsuario)" +
+            " AS docenteA" +
+            " ON docenteA.idDocente = experienciaeducativa.idDocente" +
+            " LEFT JOIN (SELECT idEE, horaInicio, horaFin, diaSemana, aula.codigoAula" +
+            " FROM horario" +
+            " JOIN aula" +
+            " ON horario.idAula = aula.idAula" +
             ") AS horarioAula" +
-            "ON horarioAula.idEE = experienciaeducativa.idEE" +
-            "LEFT JOIN participantes" +
-            "ON experienciaeducativa.idEE = participantes.idEE" +
-            "LEFT JOIN periodo" +
-            "ON experienciaeducativa.idPeriodo = periodo.idPeriodo" +
-            "WHERE experienciaeducativa.idPeriodo = {idPeriodo} AND horarioAula.diaSemana = {diaSemana} AND participantes.idAlumno = {idAlumno}";
+            " ON horarioAula.idEE = experienciaeducativa.idEE" +
+            " LEFT JOIN participantes" +
+            " ON experienciaeducativa.idEE = participantes.idEE" +
+            " LEFT JOIN periodo" +
+            " ON experienciaeducativa.idPeriodo = periodo.idPeriodo" +
+            " WHERE experienciaeducativa.idPeriodo = @idPeriodo AND horarioAula.diaSemana = @diaSemana AND participantes.idAlumno = @idAlumno";
         #endregion QUERY_RECUPERAR_HORARIO_ALUMNO_DIASEMANA
 
         #endregion QUERYS
 
 
-        public static HorarioDiaEE RecuperarHorarioAlumnoPorDia(int periodo, int diaSemana, int idAlumno)
+        public static List<HorarioDiaEE> RecuperarHorariosAlumnoPorDia(int idperiodo, int diaSemana, int idAlumno)
         {
-            HorarioDiaEE horarioDiaEE = null;
+            List<HorarioDiaEE> listaHorariosEEDia = null;
 
+            MySqlConnection connection = ConexionBD.ObtenerConexion();
+            if(connection != null)
+            {
+                try
+                {
+                    MySqlCommand command = new MySqlCommand(QUERY_RECUPERAR_HORARIO_ALUMNO_DIASEMANA, connection);
+                    command.Parameters.AddWithValue("@idPeriodo",idperiodo);
+                    command.Parameters.AddWithValue("@diaSemana", diaSemana);
+                    command.Parameters.AddWithValue("@idAlumno", idAlumno);
+                    MySqlDataReader respuesta = command.ExecuteReader();
 
+                    listaHorariosEEDia = new List<HorarioDiaEE>();
+                    while (respuesta.Read())
+                    {
+                        HorarioDiaEE horarioDiaEE = new HorarioDiaEE();
+                        horarioDiaEE.IdEE = respuesta.GetInt32(0);
+                        horarioDiaEE.NRC = respuesta.GetString(1);
+                        horarioDiaEE.NombreEE = respuesta.GetString(2);
+                        horarioDiaEE.Creditos = respuesta.GetInt32(3);
+                        horarioDiaEE.Modalidad = respuesta.GetString(4);
+                        horarioDiaEE.NombreCompletoDocente = respuesta.GetString(5) + " " + respuesta.GetString(6) + " " + respuesta.GetString(7);
+                        horarioDiaEE.HoraInicio = respuesta.GetTimeSpan(8).ToString();
+                        horarioDiaEE.HoraFin = respuesta.GetTimeSpan(9).ToString();
+                        horarioDiaEE.DiaSemana = respuesta.GetInt32(10);
+                        horarioDiaEE.CodigoAula = respuesta.GetString(11);
 
-            return horarioDiaEE;
+                        listaHorariosEEDia.Add(horarioDiaEE);
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return listaHorariosEEDia;
+                }
+            }
+
+            return listaHorariosEEDia;
         }
     }
 }
